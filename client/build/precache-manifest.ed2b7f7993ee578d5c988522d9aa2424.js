5560ac6ab0066e6b864abb06062bffe612808c49
“a_villarejo19” <a.villarejo19@gmail.com>
2019-07-14 21:33:19 -0500

    Production Test

---
+++ client/build/precache-manifest.ed2b7f7993ee578d5c988522d9aa2424.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c65b469c0a5ca0c8fee7198d94e4dc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca71d7179d92392f7f7c",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c792643698de9b49dfe7",
+    "url": "/static/js/2.7b3134ee.chunk.js"
+  },
+  {
+    "revision": "ca71d7179d92392f7f7c",
+    "url": "/static/js/main.216be799.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
